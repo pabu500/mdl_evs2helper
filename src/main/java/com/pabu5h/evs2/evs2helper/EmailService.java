01a6c879a10e6e28b6cdb72aaa1037e9456c54cc
pa596 <pa596bu500@gmail.com>
2023-08-30 13:44:45 +0800

    add from in email, use Autowired for JavaMailSender bean

--- src/main/java/com/pabu5h/evs2/evs2helper/EmailService.java
+++ src/main/java/com/pabu5h/evs2/evs2helper/EmailService.java
@@ -1,5 +1,6 @@
 package com.pabu5h.evs2.evs2helper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
@@ -7,14 +8,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class EmailService {
+
+    @Autowired
     private final JavaMailSender mailSender;
 
     public EmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
-    public void sendEmail(String to, String subject, String text) {
+    public void sendSimpleEmail(String from, String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
