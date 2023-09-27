c4f0fb5dbc826771371eb9c1ff39e19326c9ac97
ggbotbox <oliver.lu@pa.com.sg>
2023-09-27 22:38:28 +0800

    misc update

--- src/main/java/com/pabu5h/evs2/evs2helper/PaymentDef.java
+++ src/main/java/com/pabu5h/evs2/evs2helper/PaymentDef.java
@@ -2,10 +2,12 @@ package com.pabu5h.evs2.evs2helper;
 
 public class PaymentDef {
     public static int offerId = 172;
+    public static final int paymentModeVirtual = 4;
     public static final int paymentModeNetOnlineQr = 11;
     public static final int paymentModeENets = 12;
     public static final int paymentModeStripeCard = 21;
-    public static final int paymentChannel = 102; //EVS2 Consumer Portal
+    public static final int paymentChannelEvs2Ops = 101; //EVS2 Ops Portal
+    public static final int paymentChannelEvs2Consumer = 102; //EVS2 Consumer Portal
     public static double conversionRatio = 10;
     public static int isDedicated = 0;
 }
