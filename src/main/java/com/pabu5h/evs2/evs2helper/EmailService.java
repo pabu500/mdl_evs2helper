2e9f75a9c18c08f39b5b375518ee60c530b0c8d2
ggbotbox <oliver.lu@pa.com.sg>
2023-10-01 22:07:02 +0800

    add mime sender

--- src/main/java/com/pabu5h/evs2/evs2helper/EmailService.java
+++ src/main/java/com/pabu5h/evs2/evs2helper/EmailService.java
@@ -1,8 +1,10 @@
 package com.pabu5h.evs2.evs2helper;
 
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -25,6 +27,24 @@ public class EmailService {
         message.setText(text);
         try {
             mailSender.send(message);
+        }catch (Exception e){
+            logger.info("mailSender error: " + e.getMessage());
+        }
+    }
+    public void sendMimeEmail(String fromAddress, String senderName, String to, String subject, String text) {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+        try {
+            message.setFrom(senderName+ " <"+fromAddress+">");
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+
+            try {
+                mailSender.send(mimeMessage);
+            }catch (Exception e){
+                logger.info("mailSender error: " + e.getMessage());
+            }
         }catch (Exception e){
             logger.info("mailSender error: " + e.getMessage());
         }
