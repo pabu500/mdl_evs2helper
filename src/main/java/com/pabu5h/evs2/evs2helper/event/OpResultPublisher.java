24323b938fdce2b7ee5e1130a046e6f19586c9b1
ggbotbox <oliver.lu@pa.com.sg>
2024-02-03 18:02:36 +0800

    add job and event

---
+++ src/main/java/com/pabu5h/evs2/evs2helper/event/OpResultPublisher.java
@@ -0,0 +1,15 @@
+package com.pabu5h.evs2.evs2helper.event;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OpResultPublisher {
+    @Autowired
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    public void publishEvent(final OpResultEvent event) {
+        applicationEventPublisher.publishEvent(event);
+    }
+}
