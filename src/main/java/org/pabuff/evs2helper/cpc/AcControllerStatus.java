0461a9539ae798655794b54b3ff07d5db973172e
pa596 <oliver.lu@pa.com.sg>
2025-07-24 18:33:05 +0800

    add cpc

---
+++ src/main/java/org/pabuff/evs2helper/cpc/AcControllerStatus.java
@@ -0,0 +1,13 @@
+package org.pabuff.evs2helper.cpc;
+
+public enum AcControllerStatus {
+    ac_on,
+    ac_off,
+    remote_locked,
+    remote_unlocked,
+    ac_unknown,
+    remote_unknown,
+    service_on,
+    service_off,
+    service_unknown,
+}
---
+++ src/main/java/org/pabuff/evs2helper/cpc/AcLockStatus.java
@@ -0,0 +1,10 @@
+package org.pabuff.evs2helper.cpc;
+
+public enum AcLockStatus {
+    want_on,
+    on,
+    want_off,
+    off,
+    unknown
+}
+
